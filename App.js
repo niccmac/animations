169ba093522ea5ae9e0f4834318621d2e453ed77
Nicole MacLean <nicole.mac0404@gmail.com>
2023-02-21 11:55:03 -0800

    Add colour interpolation

--- App.js
+++ App.js
@@ -81,6 +81,25 @@ export default function App() {
       }).start();
     });
   };
+  const animatedColour = useRef(new Animated.Value(0)).current;
+  const boxInterpolation = animatedColour.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["rgb(154,205,50)", "rgb(255,0,0)"],
+  });
+
+  const startColourAnimation = () => {
+    Animated.timing(animatedColour, {
+      toValue: 1,
+      duration: 3000,
+      useNativeDriver: false,
+    }).start(() => {
+      Animated.timing(animatedColour, {
+        toValue: 0,
+        duration: 3000,
+        useNativeDriver: false,
+      }).start();
+    });
+  };
   return (
     <View style={styles.container}>
       <Text>Animations</Text>
@@ -185,6 +204,20 @@ export default function App() {
           }}
         >
           <Text style={{ color: "white" }}>{"ABSOLUTE POSITION"}</Text>
+        </Animated.View>
+      </TouchableWithoutFeedback>
+      <TouchableWithoutFeedback onPressIn={startColourAnimation}>
+        <Animated.View
+          style={{
+            backgroundColor: boxInterpolation,
+            flexDirection: "row",
+            height: 100,
+            width: 100,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ color: "white" }}>{"COLOUR INTERPOLATION"}</Text>
         </Animated.View>
       </TouchableWithoutFeedback>
     </View>
